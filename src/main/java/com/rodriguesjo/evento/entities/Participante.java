73536f577cbc0c3fabbeba8523e2f5f75eef4562
rodriguesjo <rodriguesjo8@outlook.com>
2025-01-28 23:00:31 -0300

    Implementação das classes, conexão com banco H2 e o mapeamento

---
+++ src/main/java/com/rodriguesjo/evento/entities/Participante.java
@@ -0,0 +1,48 @@
+package com.rodriguesjo.evento.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+    public Participante(){
+    }
+
+    public Participante(Integer id, String email, String nome) {
+        this.id = id;
+        this.email = email;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
